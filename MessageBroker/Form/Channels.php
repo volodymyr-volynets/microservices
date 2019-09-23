a68083c42d5e84679982d1403b198367a2895deb
Volodymyr Volynets <volodymyr.volynets@gmail.com>
2019-09-23 18:29:22 -0400

    Micro Services --- Micro Services:	Enhancement \#1243 \(Added channel versions. Producers and Consumers.\)

--- MessageBroker/Form/Channels.php
+++ MessageBroker/Form/Channels.php
@@ -16,6 +16,15 @@ class Channels extends \Object\Form\Wrapper\Base {
 	];
 	public $containers = [
 		'top' => ['default_row_type' => 'grid', 'order' => 100],
+		'versions_container' => [
+			'type' => 'details',
+			'details_rendering_type' => 'table',
+			'details_new_rows' => 1,
+			'details_key' => '\Numbers\Microservices\MessageBroker\Model\Channel\Versions',
+			'details_pk' => ['mb_chanversion_code'],
+			'required' => true,
+			'order' => 800,
+		],
 		'buttons' => ['default_row_type' => 'grid', 'order' => 900]
 	];
 	public $rows = [];
@@ -33,12 +42,27 @@ class Channels extends \Object\Form\Wrapper\Base {
 				'mb_channel_delay_seconds' => ['order' => 2, 'label_name' => 'Delay (seconds)', 'domain' => 'order', 'null' => true, 'percent' => 50],
 			]
 		],
+		'versions_container' => [
+			'row1' => [
+				'mb_chanversion_code' => ['order' => 1, 'label_name' => 'Version Code', 'domain' => 'version_code', 'null' => true, 'required' => true, 'percent' => 30, 'onblur' => 'this.form.submit();'],
+				'mb_chanversion_name' => ['order' => 2, 'label_name' => 'Version Name', 'domain' => 'name', 'null' => true, 'required' => true, 'percent' => 65],
+				'mb_chanversion_inactive' => ['order' => 3, 'label_name' => 'Inactive', 'type' => 'boolean', 'percent' => 5]
+			]
+		],
 		'buttons' => [
 			self::BUTTONS => self::BUTTONS_DATA_GROUP
 		]
 	];
 	public $collection = [
 		'name' => 'Channels',
-		'model' => '\Numbers\Microservices\MessageBroker\Model\Channels'
+		'model' => '\Numbers\Microservices\MessageBroker\Model\Channels',
+		'details' => [
+			'\Numbers\Microservices\MessageBroker\Model\Channel\Versions' => [
+				'name' => 'Channel Versions',
+				'pk' => ['mb_chanversion_tenant_id', 'mb_chanversion_channel_code', 'mb_chanversion_code'],
+				'type' => '1M',
+				'map' => ['mb_channel_tenant_id' => 'mb_chanversion_tenant_id', 'mb_channel_code' => 'mb_chanversion_channel_code']
+			],
+		]
 	];
 }
---
+++ MessageBroker/Model/Channel/Versions.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Numbers\Microservices\MessageBroker\Model\Channel;
+class Versions extends \Object\Table {
+	public $db_link;
+	public $db_link_flag;
+	public $module_code = 'MB';
+	public $title = 'M/B Channel Versions';
+	public $schema;
+	public $name = 'mb_channel_versions';
+	public $pk = ['mb_chanversion_tenant_id', 'mb_chanversion_channel_code', 'mb_chanversion_code'];
+	public $tenant = true;
+	public $module;
+	public $orderby;
+	public $limit;
+	public $column_prefix = 'mb_chanversion_';
+	public $columns = [
+		'mb_chanversion_tenant_id' => ['name' => 'Tenant #', 'domain' => 'tenant_id'],
+		'mb_chanversion_channel_code' => ['name' => 'Channel Code', 'domain' => 'group_code'],
+		'mb_chanversion_code' => ['name' => 'Version Code', 'domain' => 'version_code'],
+		'mb_chanversion_name' => ['name' => 'Name', 'domain' => 'name'],
+		'mb_chanversion_inactive' => ['name' => 'Inactive', 'type' => 'boolean']
+	];
+	public $constraints = [
+		'mb_channel_versions_pk' => ['type' => 'pk', 'columns' => ['mb_chanversion_tenant_id', 'mb_chanversion_channel_code', 'mb_chanversion_code']],
+		'mb_chanversion_channel_code_fk' => [
+			'type' => 'fk',
+			'columns' => ['mb_chanversion_tenant_id', 'mb_chanversion_channel_code'],
+			'foreign_model' => '\Numbers\Microservices\MessageBroker\Model\Channels',
+			'foreign_columns' => ['mb_channel_tenant_id', 'mb_channel_code']
+		],
+	];
+	public $indexes = [
+		'mb_channel_versions_fulltext_idx' => ['type' => 'fulltext', 'columns' => ['mb_chanversion_channel_code', 'mb_chanversion_code', 'mb_chanversion_name']]
+	];
+	public $history = false;
+	public $audit = false;
+	public $optimistic_lock = false;
+	public $options_map = [];
+	public $options_active = [];
+	public $engine = [
+		'MySQLi' => 'InnoDB'
+	];
+
+	public $cache = false;
+	public $cache_tags = [];
+	public $cache_memory = false;
+
+	public $data_asset = [
+		'classification' => 'client_confidential',
+		'protection' => 2,
+		'scope' => 'enterprise'
+	];
+}
